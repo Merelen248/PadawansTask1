798a62c87ce2d4c11d7dbf28c8cdbede092be72d
Merelen248 <47604458+Merelen248@users.noreply.github.com>
2019-02-13 19:14:56 +0300

    Add files via upload

---
+++ Population.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PadawansTask1
+{
+    public class Population
+    {
+        public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
+        {
+            if ((initialPopulation >=0) && (percent >= 0) && (visitors >= 0))
+            { int i = 0;
+                double population = initialPopulation;
+                while (population <= currentPopulation)
+                {
+                    i += 1;
+                    population += population * percent / 100 + visitors;
+                }
+                return i;
+            }
+             else throw new Exception("0");
+        }
+    }
+}
